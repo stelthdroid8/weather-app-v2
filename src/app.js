0d8044590a9d4c4fa1658cc2a8ff7655392c7cba
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-28 17:53:08 -0500

    add early routes

--- src/app.js
+++ src/app.js
@@ -5,10 +5,26 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+//---------
+// ROUTES
+//---------
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.send('ROOT');
 });
 
+app.get('/help', (req, res) => {
+  res.send('HELP PAGE');
+});
+
+app.get('/about', (req, res) => {
+  res.send('ABOUT');
+});
+
+app.get('/weather', (req, res) => {
+  res.send('WEATHER');
+});
+
+//express configuration
 app.listen(port, () => {
   console.log('up on port: ', port);
 });
