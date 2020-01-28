36751601bc3be051e8ac15927bfba03ffcaeb3ca
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-28 17:48:26 -0500

    configure express

---
+++ src/app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.listen(port, () => {
+  console.log('up on port: ', port);
+});
