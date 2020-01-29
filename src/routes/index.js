a95aae5429dcb46a010b2f34d954cb6e882011c4
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-29 17:26:46 -0500

    add default params after destructuring

--- src/routes/index.js
+++ src/routes/index.js
@@ -33,23 +33,26 @@ router.get('/weather', (req, res) => {
     return res.send({ error: 'You must provide an address' });
   }
 
-  geocode(req.query.address, (error, { latitude, longitude, location }) => {
-    if (error) {
-      return res.send({ error });
-    }
-
-    forecast(latitude, longitude, (error, forecastData) => {
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
       if (error) {
         return res.send({ error });
       }
 
-      res.send({
-        forecast: forecastData,
-        location,
-        address: req.query.address
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address
+        });
       });
-    });
-  });
+    }
+  );
 });
 
 // just test route
