7dbe6085bac69c3d87622b5b1fb83bade52b14fc
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-29 12:48:46 -0500

    update routes with proper objects and add new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,18 +13,36 @@ router.get('/', (req, res) => {
 
 router.get('/about', (req, res) => {
   res.render('about', {
-    title: 'About'
+    title: 'About',
+    name: 'Brandon Kennedy'
   });
 });
 
 router.get('/help', (req, res) => {
   res.render('help', {
-    title: 'Help'
+    title: 'Help',
+    name: 'Brandon Kennedy'
   });
 });
 
 router.get('/weather', (req, res) => {
   res.send('WEATHER');
+});
+
+router.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    error: 'Help article not found',
+    name: 'Brandon Kennedy'
+  });
+});
+
+router.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    error: 'Page not found.',
+    name: 'Brandon Kennedy'
+  });
 });
 
 module.exports = router;
