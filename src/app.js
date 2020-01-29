bbeb0636ee5fc9aef76e1384b0c48ac582b491a6
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-28 19:23:12 -0500

    config express for templates and add comments

--- src/app.js
+++ src/app.js
@@ -1,20 +1,28 @@
 require('dotenv').config();
-
 const express = require('express');
 const path = require('path');
 
-const indexRoutes = require('./routes/index');
-
-const port = process.env.PORT || 3000;
-const publicDir = path.join(__dirname, '../public');
-
 const app = express();
 
-app.set('view engine', 'hbs');
+//require in routes
+const indexRoutes = require('./routes/index');
 
+//set up port recognition
+const port = process.env.PORT || 3000;
+
+//define paths for express config
+const publicDir = path.join(__dirname, '../public');
+const viewsPathDir = path.join(__dirname, '../templates');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPathDir);
+
+//setup static directory to serve
 app.use(express.static(publicDir));
 
+//use routes
 app.use(indexRoutes);
+
 //---------------------
 //express configuration
 app.listen(port, () => {
