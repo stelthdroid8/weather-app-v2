8a547f23169d6caf2ba57ccd293d38e8b453a605
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-29 15:45:27 -0500

    add support to query mapbox and darksky API

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const forecast = require('../utils/forecast');
+const geocode = require('../utils/geocode');
+
 router = express.Router();
 
 //---------
@@ -26,8 +29,37 @@ router.get('/help', (req, res) => {
 });
 
 router.get('/weather', (req, res) => {
-  res.send('WEATHER');
+  if (!req.query.address) {
+    return res.send({ error: 'You must provide an address' });
+  }
+
+  geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return res.send({ error });
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address
+      });
+    });
+  });
 });
+
+// just test route
+// router.get('/products', (req, res) => {
+//   if (!req.query.search) {
+//     return res.send({ error: 'You must provide a search term.' });
+//   }
+
+//   res.send({ products: [] });
+// });
 
 router.get('/help/*', (req, res) => {
   res.render('404', {
