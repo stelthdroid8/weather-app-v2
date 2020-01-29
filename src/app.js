62db19eb5f3b9bba07a85a5056d49f48c724dd0b
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-29 12:13:10 -0500

    rearrange folder structure

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-
+const hbs = require('hbs');
 const app = express();
 
 //require in routes
@@ -12,10 +12,12 @@ const port = process.env.PORT || 3000;
 
 //define paths for express config
 const publicDir = path.join(__dirname, '../public');
-const viewsPathDir = path.join(__dirname, '../templates');
+const viewsPathDir = path.join(__dirname, '../templates/views');
+const partialDir = path.join(__dirname, '../templates/partials');
 
 app.set('view engine', 'hbs');
 app.set('views', viewsPathDir);
+hbs.registerPartials(partialDir);
 
 //setup static directory to serve
 app.use(express.static(publicDir));
